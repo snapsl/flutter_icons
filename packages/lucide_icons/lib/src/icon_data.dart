a28fc3433155d01ec73596bd936ba8788e76aa4c
snapsl <snapsl@users.noreply.github.com>
2026-01-08 13:38:09 +0100

    add lucide icons package

---
+++ packages/lucide_icons/lib/src/icon_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+/// Constructor for [IconData] which includes the font family
+class LucideIconData extends IconData {
+  /// Constructor for [IconData] which includes the font family
+  const LucideIconData(super.codePoint)
+    : super(fontFamily: 'LucideIcons', fontPackage: 'lucide_icons');
+}
