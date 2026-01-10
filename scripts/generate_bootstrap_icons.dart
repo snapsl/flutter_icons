9c0bd810cdbfd37ceec01cb055722cdf71daae7d
snapsl <snapsl@users.noreply.github.com>
2026-01-10 16:52:50 +0100

    remove comment

--- scripts/generate_bootstrap_icons.dart
+++ scripts/generate_bootstrap_icons.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 
 import 'package:generator/generator.dart';
 
-// TODO npm has woff file. need conversion to ttf
-
 void main() {
   const path = './vendors/bootstrap_icons/node_modules/bootstrap-icons';
 
