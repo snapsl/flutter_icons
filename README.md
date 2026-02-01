f901f60083664b9891ad048e774fba4fd52f7333
snapsl <snapsl@users.noreply.github.com>
2026-02-01 17:29:34 +0100

    add ionicons

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ This project contains a collection of icon sets for Flutter. The icons are gener
 | [Ant Design Icons](https://ant.design/components/icon) | [![pub package](https://img.shields.io/badge/version-placeholder-blue)](https://pub.dev/packages/flutter_icons/ant_design_icons) |
 | [Bootstrap Icons](https://icons.getbootstrap.com) | [![pub package](https://img.shields.io/badge/version-placeholder-blue)](https://pub.dev/packages/flutter_icons/bootstrap_icons) |
 | [Hero Icons](https://heroicons.com) | [![pub package](https://img.shields.io/badge/version-placeholder-blue)](https://pub.dev/packages/flutter_icons/heroicons) |
+| [Ionic Icons](https://ionic.io/ionicons) | [![pub package](https://img.shields.io/badge/version-placeholder-blue)](https://pub.dev/packages/flutter_icons/ionic_icons) |
 | [Lucide Icons](https://lucide.dev/icons) | [![pub package](https://img.shields.io/badge/version-placeholder-blue)](https://pub.dev/packages/flutter_icons/lucide_icons) |
 | [Remix Icons](https://remixicon.com) | [![pub package](https://img.shields.io/badge/version-placeholder-blue)](https://pub.dev/packages/flutter_icons/remix_icons) |
 | [Simple Icons](https://simpleicons.org) | [![pub package](https://img.shields.io/badge/version-placeholder-blue)](https://pub.dev/packages/flutter_icons/simple_icons) |
---
+++ packages/ionic_icons/test/icon_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ionic_icons/src/icon_data.g.dart';
+import 'package:test_utils/test_utils.dart';
+
+void main() {
+  const icon = IonicIcons.alarm;
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    await loadFonts(
+      iconMap: {'fonts/ionicons.ttf': icon},
+      packageName: 'iocnic_icons',
+    );
+
+    final testUrl = (goldenFileComparator as LocalFileComparator).basedir;
+    goldenFileComparator = TolerantGoldenFileComparator(
+      testUrl,
+      precisionTolerance: 0.01,
+    );
+  });
+
+  group('Ionic Icons Tests', () {
+    testWidgets('Widget test', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: Icon(icon)));
+
+      final iconFinder = find.byIcon(icon);
+      expect(iconFinder, findsOneWidget);
+
+      final iconWidget = tester.widget<Icon>(iconFinder);
+
+      expect(iconWidget.icon, isNotNull);
+      expect(iconWidget.icon!.fontFamily, 'IonicIcons');
+    });
+
+    testWidgets('Golden test', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Icon(
+            size: 240,
+            IconData(icon.codePoint, fontFamily: icon.fontFamily),
+          ),
+        ),
+      );
+
+      await expectLater(
+        find.byType(Icon),
+        matchesGoldenFile('goldens/icon.png'),
+      );
+    });
+  });
+}
